7ea59162b23ff57c9ff1773f97c40049643604e4
sunxu2 <1017294721@qq.com>
2018-08-27 13:11:59 +0800

    333

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   BASE_API: '"https://api-dev"',
-  TEST_API: '"http://192.168.1.224:9011"'
+  TEST_API: '"http://119.29.108.164:9011"'
 }
